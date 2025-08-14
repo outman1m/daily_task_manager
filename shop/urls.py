bc5782562ece5cd8dd65dbe95563059b17266604
outman1m <outman.talbi200@gmail.com>
2025-08-14 12:39:17 -0700

    Initial Django project setup

---
+++ shop/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ProductViewSet, OrderViewSet, OrderItemViewSet
+
+router = DefaultRouter()
+router.register(r'products', ProductViewSet)
+router.register(r'orders', OrderViewSet)
+router.register(r'order-items', OrderItemViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
